bc4a312f4e64b99c08881a47714958ccb49aa906
kvs0102013 <kvs0102013@gmail.com>
2025-06-01 13:13:56 +0300

    ls_4.2_ts_2

--- lesson_4.1.py
+++ lesson_4.1.py
@@ -1,9 +1,18 @@
-a = int(input('Введите первое число: '))
-b = int(input('Введите второе число: '))
-print(a, b)
-a = a + b
-print('a = ', a)
-b = a - b
-print('b = ', b) 
-a = a - b 
-print(a,b)
+# a = int(input('Введите первое число: '))# Поменять числа а и б местами
+# b = int(input('Введите второе число: '))
+# print(a, b)
+# a = a + b
+# print('a = ', a)
+# b = a - b
+# print('b = ', b) 
+# a = a - b 
+# print(a,b) 
+
+number =  1234 #Четырех значное число наоборот
+a = number % 10
+b = number % 100 // 10
+c = number // 100 % 10
+d = number // 1000
+print(a,b,c,d)
+revers_number = a * 1000 + b * 100 + c * 10 + d 
+print( 'Число наоборот:' , revers_number )
