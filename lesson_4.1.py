6227c463f3cca12a7c7e73fa11c52a4ccb635671
kvs0102013 <kvs0102013@gmail.com>
2025-06-09 15:30:56 +0300

    task_4.2

--- lesson_4.1.py
+++ lesson_4.1.py
@@ -8,11 +8,24 @@
 # a = a - b 
 # print(a,b) 
 
-number =  1234 #Четырех значное число наоборот
-a = number % 10
-b = number % 100 // 10
-c = number // 100 % 10
-d = number // 1000
-print(a,b,c,d)
-revers_number = a * 1000 + b * 100 + c * 10 + d 
-print( 'Число наоборот:' , revers_number )
+# number =  1234 #Четырех значное число наоборот
+# a = number % 10
+# b = number % 100 // 10
+# c = number // 100 % 10
+# d = number // 1000
+# print(a,b,c,d)
+# revers_number = a * 1000 + b * 100 + c * 10 + d 
+# print( 'Число наоборот:' , revers_number )
+
+# mon = int(input('Количество денег на счету  '))
+# kurs = 75000
+# if mon >= kurs:
+#     print('Курс успешно приобретен')
+# print('Хорошего дня')
+ 
+ # проверка четности числа   
+number = int(input("Введите число: "))
+if number % 2 == 0:
+    print("Число чётное")    
+else:
+    print(number , 'Не четное число')    
