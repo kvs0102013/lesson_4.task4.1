e5419e962d9debd3b9838712662360b66e882994
kvs0102013 <kvs0102013@gmail.com>
2025-06-10 14:42:38 +0300

    task_4.3.3

---
+++ lesson_4.3.py
@@ -0,0 +1,19 @@
+# first_dig = int(input('Введите первое число:'))
+# second_dig = int(input('Введите второе число:'))
+# summ_dig = int(input('Введите прелпологаемую сумму чисел:'))
+# summ = first_dig + second_dig
+# if summ == summ_dig:
+#     print('Ответ верный')
+# else:
+#     print('Ответ не верный')
+#     print('Верный ответ ' , summ)
+         
+print("Добро пожаловать в игру «Угадай число»!")
+print("Настя загадывает число. Дима, не подглядывай!")
+nastya_number = int(input("Введите число: "))
+dima_number = int(input("Введите число: "))
+
+if nastya_number == dima_number:
+    print("Ура! Угадал!")
+else:
+    print("Нет, это не то, что я загадала.")    
